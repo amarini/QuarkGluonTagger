74801217e6f5d5af1088104e1683bf79a2a35bc2
Andrea Marini <andrea.marini@cern.ch>
2013-06-21 12:17:21 +0200

    changed Produce Plots in order to use one template function

--- EightTeV/src/ProducePlots.sh
+++ EightTeV/src/ProducePlots.sh
@@ -1,57 +1,46 @@
 #!/bin/bash
 
 FILE=../data/SystDatabase.txt
+FILE=./SystZJet.txt
 mkdir -p ~/work/PlotQG/
 
-do_dijet()
+
+template() # parameters: $1=TYPE=DiJet,ZJet,ZJet2 
 {
 linenumber=0
-cat ${FILE} | grep -v '^#' | while read varname ptmin ptmax rhomin rhomax etamin etamax alpha beta lmin lmax ; do
+type1=$1;
+type2=$1;
+[ $type2 == "ZJet" ] && type2="";
+cat ${FILE} | grep -v '^#' | grep -v '^$' | while read varname ptmin ptmax rhomin rhomax etamin etamax alpha beta lmin lmax ; do
 ((linenumber++))
-[ $etamax -eq 5 ] && etamax=4.7;
-echo "Executing line DiJet $linenumber"
+[ "$etamax" == "5" ] && etamax="4.7";
+[ "$etamax" == "5.0" ] && etamax="4.7";
+echo "Executing line $type1 $linenumber - ComputeDoubleMin$type2"
 root  -l -b <<EOF
-.L ComputeDoubleMinDiJet.C+
+.L ComputeDoubleMin${type2}.C+
 TCanvas *c;
 Check(${ptmin},${ptmax},${rhomin},${rhomax},${etamin},${etamax},${alpha},${beta},"${varname}",${lmin},${lmax},&c);
-c->SaveAs("~/work/PlotQG/DiJetSystValidation_${varname}_pt${ptmin}_${ptmax}_eta${etamin}_${etamax}_rho${rhomin}_${rhomax}.pdf")
+c->SaveAs("~/work/PlotQG/${type}SystValidation_${varname}_pt${ptmin}_${ptmax}_eta${etamin}_${etamax}_rho${rhomin}_${rhomax}.pdf")
+c->SaveAs("~/work/PlotQG/${type}SystValidation_${varname}_pt${ptmin}_${ptmax}_eta${etamin}_${etamax}_rho${rhomin}_${rhomax}.root")
 .q
 EOF
 done
+
+}
+
+do_dijet()
+{
+template DiJet ;
 }
 
 do_zjet()
 {
-linenumber=0
-cat ${FILE} | grep -v '^#' | while read varname ptmin ptmax rhomin rhomax etamin etamax alpha beta lmin lmax ; do
-((linenumber++))
-[ $etamax -eq 5 ] && etamax=4.7;
-echo "Executing line ZJet $linenumber: varname=${varname} pt=[${ptmin},${ptmax}] rho=[${rhomin},${rhomax}] eta=[$etamin,$etamax] par=[$alpha,$beta,$lmin,$lmax]"
-root  -l -b <<EOF
-.L ComputeDoubleMin.C+
-TCanvas *c;
-Check(${ptmin},${ptmax},${rhomin},${rhomax},${etamin},${etamax},${alpha},${beta},"${varname}",${lmin},${lmax},&c);
-c->SaveAs("~/work/PlotQG/ZJetSystValidation_${varname}_pt${ptmin}_${ptmax}_eta${etamin}_${etamax}_rho${rhomin}_${rhomax}.pdf")
-.q
-EOF
-done
+template ZJet ;
 }
 
 do_zjet2()
 {
-linenumber=0
-cat ${FILE} | grep -v '^#' | while read varname ptmin ptmax rhomin rhomax etamin etamax alpha beta lmin lmax ; do
-((linenumber++))
-[ $etamax -eq 5 ] && etamax=4.7;
-echo "Executing line ZJet2 $linenumber"
-root  -l -b <<EOF
-.L ComputeDoubleMinZJet2.C+
-TCanvas *c;
-Check(${ptmin},${ptmax},${rhomin},${rhomax},${etamin},${etamax},${alpha},${beta},"${varname}",${lmin},${lmax},&c);
-c->SaveAs("~/work/PlotQG/ZJetHSSystValidation_${varname}_pt${ptmin}_${ptmax}_eta${etamin}_${etamax}_rho${rhomin}_${rhomax}.pdf")
-.q
-EOF
-done
+template ZJet2 ;
 }
 
 check_parsing()
@@ -67,3 +56,5 @@ done
 
 check_parsing;
 do_zjet;
+#do_zjet2;
+#do_dijet;
